590dcbe9d69dcbfec67a0282fb59e6de890e06aa
Diane Crame <dcrame@icloud.com>
2020-03-26 18:20:36 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,12 +8,13 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
-    winner
   end
+  winner
 end
 
 select_winner(passengers)
