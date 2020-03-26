420e16093252ef6d8d31cf0248e907313396f207
Diane Crame <dcrame@icloud.com>
2020-03-26 18:32:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,8 @@ happy_birthday(birthday_kids)
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    age > 12 ? puts "Happy Birthday #{name}! You are now #{age} years old!" 
+    if age > 12  
+      puts "Happy Birthday #{name}! You are now #{age} years old!" 
   end
+end
 end
